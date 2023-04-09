0dd240ea31d44501a207cb33559d9526f5a1f77a
nikagoci <86802785+nikagoci@users.noreply.github.com>
2023-04-09 11:58:43 +0400

    initialize project

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        green: '#64ffda',
+        slate: '#8892b0',
+        lightslate: '#ccd6f6',
+        lightgreen: 'rgba(100,255,218,0.1)'
+      }
     },
   },
   plugins: [],
